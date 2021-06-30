df7ea9492053b36424c7b87b8c1f2c3f7c83036e
Matthew Hamilton <mhamilt7@ed.ac.uk>
2021-06-30 14:58:39 +0100

    trying to get to the bottom of access violations

--- WinBLECentral/WinBleCentral.cpp
+++ WinBLECentral/WinBleCentral.cpp
@@ -22,13 +22,20 @@ void WinBleCentral::stop()
     bleWatcher.Stop();
 }
 //--------------------------------------------------------------------------------------------
-void WinBleCentral::connectToFoundDevice(int deviceIndex) {}
+void WinBleCentral::connectToFoundDevice(int deviceIndex)
+{
+
+    uint64_t deviceAddress = discoveredPeripherals[deviceIndex].BluetoothAddress();
+    std::cout << std::hex << "Connect to: " << deviceAddress << std::endl;
+    connectPeripheral(deviceAddress);
+}
 
 //--------------------------------------------------------------------------------------------
-void WinBleCentral::connectToDeviceWithUUID(const char* uuid) 
+void WinBleCentral::connectToDeviceWithUUID(const char* uuid)
 {
-    uint64_t deviceAddress;
-    BluetoothLEDevice::FromBluetoothAddressAsync(deviceAddress);
+    // for items in dicovered peripherals
+    // if item.uuid equals uuid
+    // connectToPeripheral(item.BluetoothAddress());
 }
 
 //--------------------------------------------------------------------------------------------
@@ -53,40 +60,156 @@ std::string WinBleCentral::winrtGuidToString(winrt::guid uuid)
 {
     char uuidCStr[34];
     uint64_t d6 = *((uint64_t*)&uuid.Data4[2]);
-    sprintf(uuidCStr, "%08x-%04x-%04x-%02x%02x-%06x", uuid.Data1, uuid.Data2, uuid.Data3, uuid.Data4[0], uuid.Data4[1], d6);
+    if(uuid.Data2 == 0)
+        sprintf(uuidCStr, "%04x", uuid.Data1);
+    else
+        sprintf(uuidCStr, "%08x-%04x-%04x-%02x%02x-%06x", uuid.Data1, uuid.Data2, uuid.Data3, uuid.Data4[0], uuid.Data4[1], d6);
     std::string guid = std::string(uuidCStr);
     return guid;
+}
+//--------------------------------------------------------------------------------------------
+void WinBleCentral::connectPeripheral(uint64_t windowsDeviceAddress)
+{
+    /*
+    * Potential Source of bother.
+    https://devblogs.microsoft.com/oldnewthing/20201030-00/?p=104409
+
+    use https://docs.microsoft.com/en-us/uwp/api/windows.foundation.metadata.apiinformation.ismethodpresent?view=winrt-20348
+
+    to test
+    */
+
+  //  this->didConnectPeripheral(BluetoothLEDevice::FromBluetoothAddressAsync(windowsDeviceAddress).get());
+    auto awaitBluetooth = BluetoothLEDevice::FromBluetoothAddressAsync(127957777215202);
+    BluetoothLEDevice nRF = awaitBluetooth.get();
+
+    auto waiting = nRF.GetGattServicesAsync();
+    auto result = waiting.get();
+
+    auto bruh = nRF.GetGattServicesAsync();
+    auto d = bruh.get();
+    auto services = d.Services();
+    for (auto service : services)
+    {
+        std::cout << "Service: " << winrtGuidToString(service.Uuid()) << std::endl;
+    }
+    //BluetoothLEDevice::FromBluetoothAddressAsync(windowsDeviceAddress).Completed(
+    //    [this](IAsyncOperation<BluetoothLEDevice> sender, AsyncStatus status)
+    //    {
+    //        auto device = sender.GetResults();
+    //        switch (status)
+    //        {
+    //        case winrt::Windows::Foundation::AsyncStatus::Completed:
+    //            this->didConnectPeripheral(device);
+    //            break;
+    //        case winrt::Windows::Foundation::AsyncStatus::Canceled:
+    //        case winrt::Windows::Foundation::AsyncStatus::Error:
+    //        case winrt::Windows::Foundation::AsyncStatus::Started:
+    //            this->didFailToConnectPeripheral();
+    //        }
+    //    });
+}
+
+void WinBleCentral::discoverServices(BluetoothLEDevice device)
+{
+    /*auto async = device.GetGattServicesAsync();
+    if (async.wait_for(5s) == AsyncStatus::Completed)
+    {
+        didDiscoverServices(async.GetResults().Services(), async.GetResults().Status());
+    }*/
+
+
+    //this->didDiscoverServices(result.Services(), result.Status());
+    //worker.Completed(
+    //    [this](IAsyncOperation<GattDeviceServicesResult>sender, AsyncStatus status)
+    //    {
+    //        //if (status == AsyncStatus::Completed)
+    //        //{
+    //        //    //GattDeviceServicesResult result = sender.get();
+    //        //   // sender.get();
+    //        //    //this->didDiscoverServices(result.Services(), result.Status());
+    //        //}
+    //        //else
+    //        //{
+    //        //    this->didFailToDiscoverServices();
+    //        //}
+    //    });
 }
 //--------------------------------------------------------------------------------------------
 void WinBleCentral::didDiscoverPeripheral(BluetoothLEAdvertisementWatcher watcher,
     BluetoothLEAdvertisementReceivedEventArgs eventArgs)
 {
-    if (isPeripheralNew(eventArgs))
+
+    if (isPeripheralNew(eventArgs) && eventArgs.RawSignalStrengthInDBm() > -60 && 127957777215202 == eventArgs.BluetoothAddress())
     {
         discoveredPeripheralUUIDs.push_back(eventArgs.BluetoothAddress());
         discoveredPeripherals.push_back(eventArgs);
-
+        bleWatcher.Stop();
         if (shouldReport)
         {
             std::cout << "Device UUID: " << eventArgs.BluetoothAddress() << std::endl;
             printDeviceDescription(eventArgs);
         }
+        
+        connectToFoundDevice(discoveredPeripherals.size() - 1);
     }
 }
 //--------------------------------------------------------------------------------------------
-void WinBleCentral::didConnectPeripheral() {  }
+void WinBleCentral::didConnectPeripheral(BluetoothLEDevice &device)
+{
+    if (!connecting)
+    {
+        
+        this->connecting = true;
+        std::wcout << "didConnectPeripheral" << std::endl;
+    }
+}
 
 //--------------------------------------------------------------------------------------------
 void WinBleCentral::didDisconnectPeripheral() {}
 
 //--------------------------------------------------------------------------------------------
-void WinBleCentral::didFailToConnectPeripheral() {}
-
+void WinBleCentral::didFailToConnectPeripheral()
+{
+    std::cout << "didFailToConnectPeripheral" << std::endl;
+}
+void WinBleCentral::didFailToDiscoverServices()
+{
+    std::cout << "didFailToDiscoverServices" << std::endl;
+}
 //--------------------------------------------------------------------------------------------
 void WinBleCentral::didDiscoverIncludedServicesForService() {}
 
 //--------------------------------------------------------------------------------------------
-void WinBleCentral::didDiscoverServices() {}
+void WinBleCentral::didDiscoverServices(IVectorView<GattDeviceService> services, GattCommunicationStatus status)
+{
+    std::cout << "didDiscoverServices" << std::endl;
+
+    if (status == GattCommunicationStatus::Success)
+    {
+        for (auto service : services)
+        {
+            std::cout << "Service: " << winrtGuidToString(service.Uuid()) << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Error Getting Services: ";
+        switch (status)
+        {
+        case winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCommunicationStatus::Unreachable:
+            std::cout << "Unreachable";
+            break;
+        case winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCommunicationStatus::ProtocolError:
+            std::cout << "ProtocolError";
+            break;
+        case winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattCommunicationStatus::AccessDenied:
+            std::cout << "AccessDenied";
+            break;
+        }
+        std::cout << std::endl;
+    }
+}
 
 //--------------------------------------------------------------------------------------------
 void WinBleCentral::didDiscoverCharacteristicsForService() {}
@@ -107,12 +230,12 @@ void WinBleCentral::printDeviceDescription(BluetoothLEAdvertisementReceivedEvent
 {
     BluetoothLEAdvertisement deviceAdvert = device.Advertisement();
     std::cout << "Name: " << deviceAdvert.LocalName().c_str() << std::endl;
-    
-    for (auto service: deviceAdvert.ServiceUuids())            
-        std::cout << "UUID: " << winrtGuidToString(service) << std::endl;        
-     
+
+    for (auto service : deviceAdvert.ServiceUuids())
+        std::cout << "UUID: " << winrtGuidToString(service) << std::endl;
+
     for (auto manuData : deviceAdvert.ManufacturerData())
-    {       
+    {
         std::cout << std::hex << "Manu: ";
         printf("%04x : ", manuData.CompanyId());
 
@@ -122,6 +245,7 @@ void WinBleCentral::printDeviceDescription(BluetoothLEAdvertisementReceivedEvent
         }
         std::cout << std::endl;
     }
+    std::cout << std::dec <<  "RSSI: " << device.RawSignalStrengthInDBm() <<  std::endl;
     std::cout << "*----------------------------------------*" << std::endl;
 }
 //--------------------------------------------------------------------------------------------
@@ -130,3 +254,29 @@ bool WinBleCentral::isPeripheralNew(BluetoothLEAdvertisementReceivedEventArgs ev
     return (discoveredPeripheralUUIDs.empty() || !(std::find(discoveredPeripheralUUIDs.begin(), discoveredPeripheralUUIDs.end(), eventArgs.BluetoothAddress()) != discoveredPeripheralUUIDs.end()));
 }
 
+
+
+void oldGetDeviceInfo()
+{
+    //auto gattServices = device.GetGattServicesAsync(BluetoothCacheMode::Uncached).get(); // https://docs.microsoft.com/en-us/cpp/parallel/concrt/reference/task-class?view=msvc-160#get
+
+    //std::cout << gattServices.Services().Size() << std::endl;
+
+    //std::array<uint8_t, 8> ar = { 17,17,17,17,17,17,17,17 };
+    //std::array<uint8_t, 8> ar2 = { 34,34,34,34,34,34,34,34 };
+
+    //winrt::guid Guid((uint32_t)0x111111111, (uint16_t)0x1111, (uint16_t)0x1111, ar);
+    //winrt::guid charGuid((uint32_t)0x22222222, (uint16_t)0x2222, (uint16_t)0x2222, ar2);
+
+    //auto bruh = device.GetGattServicesForUuidAsync(Guid);
+    //auto d = bruh.get();
+    //auto services = d.Services();
+    //std::cout << "Service Size: " << services.Size() << std::endl;
+    //std::cout << "Char Size: " << services.GetAt(0).GetCharacteristicsForUuidAsync(charGuid).get().Characteristics().Size() << std::endl;
+    // DataWriter f;
+    // f.WriteInt16(102);
+    // auto b = services.GetAt(0).GetCharacteristicsForUuidAsync(charGuid).get().Characteristics().GetAt(0).WriteValueAsync(f.DetachBuffer());
+
+    // auto re = services.GetAt(0).GetCharacteristicsForUuidAsync(charGuid).get().Characteristics().GetAt(0).CharacteristicProperties();
+    // std::cout << (int)re << std::endl;
+}
--- WinBLECentral/WinBleCentral.h
+++ WinBLECentral/WinBleCentral.h
@@ -21,8 +21,8 @@ using winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattSession;
 using winrt::Windows::Storage::Streams::DataWriter;
 using winrt::Windows::Devices::Bluetooth::GenericAttributeProfile::GattWriteOption;
 using winrt::Windows::Devices::Bluetooth::BluetoothCacheMode;
-
-
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Foundation::Collections;
 
 
 class WinBleCentral
@@ -49,6 +49,8 @@ public:
         int deviceIndex);
     std::string winrtGuidToString(winrt::guid);
 private:
+    void connectPeripheral(uint64_t windowsDeviceAddress);
+    void discoverServices(BluetoothLEDevice device);
     /// <summary>
     /// 
     /// </summary>
@@ -56,11 +58,12 @@ private:
     /// <param name="eventArgs"></param>
     void didDiscoverPeripheral(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs);
     void didCancelScanning();
-    void didConnectPeripheral();
+    void didConnectPeripheral(BluetoothLEDevice &device);
     void didDisconnectPeripheral();
     void didFailToConnectPeripheral();
+    void didFailToDiscoverServices();
     void didDiscoverIncludedServicesForService();
-    void didDiscoverServices();
+    void didDiscoverServices(IVectorView<GattDeviceService> services, GattCommunicationStatus error);
     void didDiscoverCharacteristicsForService();
     void didUpdateValueForDescriptor();
     void didUpdateValueForCharacteristic();
@@ -76,5 +79,91 @@ private:
     uint32_t connectDeviceIndex;
     int rssiSensitivity;
     BluetoothLEAdvertisementWatcher bleWatcher;
+    bool connecting = false;
 };
 
+#include <string_view>
+// https://gist.github.com/kennykerr/6c948882de395c25b3218ad8d4daf362
+using namespace std::literals;
+
+struct guid
+{
+    uint32_t Data1;
+    uint16_t Data2;
+    uint16_t Data3;
+    uint8_t  Data4[8];
+};
+
+constexpr bool operator==(guid const& left, guid const& right) noexcept
+{
+    return left.Data1 == right.Data1 &&
+        left.Data2 == right.Data2 &&
+        left.Data3 == right.Data3 &&
+        left.Data4[0] == right.Data4[0] &&
+        left.Data4[1] == right.Data4[1] &&
+        left.Data4[2] == right.Data4[2] &&
+        left.Data4[3] == right.Data4[3] &&
+        left.Data4[4] == right.Data4[4] &&
+        left.Data4[5] == right.Data4[5] &&
+        left.Data4[6] == right.Data4[6] &&
+        left.Data4[7] == right.Data4[7];
+}
+
+
+constexpr uint32_t to_uint(char const value) noexcept
+{
+    if (value >= '0' && value <= '9')
+    {
+        return value - '0';
+    }
+
+    if (value >= 'A' && value <= 'F')
+    {
+        return 10 + value - 'A';
+    }
+
+    if (value >= 'a' && value <= 'f')
+    {
+        return 10 + value - 'a';
+    }
+
+    std::terminate();
+}
+
+constexpr guid make_guid(std::string_view const& value) noexcept
+{
+    if (value.size() != 36 || value[8] != '-' || value[13] != '-' || value[18] != '-' || value[23] != '-')
+    {
+        std::terminate();
+    }
+
+    return
+    {
+        ((to_uint(value[0]) * 16 + to_uint(value[1])) << 24) +
+        ((to_uint(value[2]) * 16 + to_uint(value[3])) << 16) +
+        ((to_uint(value[4]) * 16 + to_uint(value[5])) << 8) +
+         (to_uint(value[6]) * 16 + to_uint(value[7])),
+
+        static_cast<uint16_t>(((to_uint(value[9]) * 16 + to_uint(value[10])) << 8) +
+        (to_uint(value[11]) * 16 + to_uint(value[12]))),
+
+        static_cast<uint16_t>(((to_uint(value[14]) * 16 + to_uint(value[15])) << 8) +
+         (to_uint(value[16]) * 16 + to_uint(value[17]))),
+
+        {
+            static_cast<uint8_t>(to_uint(value[19]) * 16 + to_uint(value[20])),
+            static_cast<uint8_t>(to_uint(value[21]) * 16 + to_uint(value[22])),
+
+            static_cast<uint8_t>(to_uint(value[24]) * 16 + to_uint(value[25])),
+            static_cast<uint8_t>(to_uint(value[26]) * 16 + to_uint(value[27])),
+            static_cast<uint8_t>(to_uint(value[28]) * 16 + to_uint(value[29])),
+            static_cast<uint8_t>(to_uint(value[30]) * 16 + to_uint(value[31])),
+            static_cast<uint8_t>(to_uint(value[32]) * 16 + to_uint(value[33])),
+            static_cast<uint8_t>(to_uint(value[34]) * 16 + to_uint(value[35])),
+        }
+    };
+}
+static constexpr guid b
+{
+    make_guid("8AA90CAD-fed1-4c54-89db-9B7522D8AA92"sv) 
+};
